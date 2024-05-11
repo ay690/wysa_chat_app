ead66f0a634d4f5c9317730bf4f085d812291b79
Aniket Yadav <aniketyadav690@gmail.com>
2024-05-11 14:39:33 +0530

    update: themepicker

---
+++ src/components/ThemePicker/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+    themeSetter:string
+  }
+
+const ThemePicker: React.FC<Props> = () => {
+  return (
+    <div>
+      Theme
+    </div>
+  )
+}
+
+export default ThemePicker
